c0a21dae479381ec1cd116af20c5f039d4922269
Arman <armanbijoy4@gmail.com>
2024-05-29 23:51:43 +0600

    Expenseitem component Added

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,17 @@
+const Expenseitem = () => {
+    return (
+      <div>
+        <div>May 29th 2024</div>
+  
+        <div>
+          <h2>Car Insurance</h2>
+  
+          {/* Amount Div */}
+          <div>$294.67</div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Expenseitem;
+  
